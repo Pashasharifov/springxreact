e29c2ba0fc5a9f5deffc0c7273ea14beca4c3325
Pashasharifov <sharifffov@gmail.com>
2025-08-18 19:08:28 +0400

    ch

---
+++ ui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EmployeeList from './components/EmployeeList';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <EmployeeList/>
+    </div>
+  );
+}
+
+export default App;
